c181711416995dbfffeb340fc858337183204a02
Artem Morozov <moroz.20000@gmail.com>
2021-03-29 00:01:56 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ const form = document.querySelector('.popup__form');
 const popupButton = document.querySelector('.popup__button');
 const test =Array.from(form.querySelectorAll('.test'));
 const headerPhone = document.querySelector('.header__menu_phone');
-
-
+const popupMenuPhone = document.querySelector('.popup__menu');
+const menuButtonPhone = document.querySelector('.header__menu_phone-button');
+const buttonOP = document.querySelector('.header__menu_phone-buttonClose');
+const kosmet = document.querySelectorAll('.header__menu_item');
+const indi = document.querySelectorAll('.header__menu_down-indik');
+const menu =Array.from(document.querySelectorAll('.header__menu_down'));
+const bgBlur = document.querySelector('.bg-blur');
+const imputs = document.querySelectorAll('.popup__input');
+const check = document.querySelector('.popup__input_check');
+//Валидация
 function someInput(){
     return !test.some((inp)=>!inp.validity.valid);
 }
-
 function validate (elem,inp){
     document.querySelectorAll('.popup__error');
     if(elem.validity.valid){
@@ -35,8 +42,9 @@ function validate (elem,inp){
         })
     };
 }
-
+// Открытие формы и присваиваине обработчиков
 function openPopup(selec){
+    headerPhone.style.background = 'white';
     if(window.innerWidth<=961){
         headerPhone.classList.add('header__menu_phone-white');
     }
@@ -45,11 +53,6 @@ function openPopup(selec){
         if(evt.key=='Escape')
         closePopup(popup);
     });
-
-    const imputs = document.querySelectorAll('.popup__input');
-    const textAr =document.querySelector('.popup__input_text');
-    const check = document.querySelector('.popup__input_check');
-
     imputs.forEach(element => {
         element.addEventListener('input',()=>{
             const inp = element.classList;
@@ -65,20 +68,6 @@ function openPopup(selec){
             validate(element,inp[0]);
         });
     });
-
-    textAr.addEventListener('input',()=>{
-        const inp = textAr.classList;
-        validate(textAr,inp[0]);
-        if(someInput()){
-            console.log(popupButton);
-            popupButton.classList.remove('popup__button_disablet');
-            popupButton.removeAttribute("disabled");
-        }
-        else{
-            popupButton.classList.add('popup__button_disablet');
-            popupButton.setAttribute("disabled", true);
-        }
-    });
     check.addEventListener('input', ()=>{
         if(someInput()){
             console.log(popupButton);
@@ -91,7 +80,9 @@ function openPopup(selec){
         }
     })
 };
+//Закрытие формы
 function closePopup(selec){
+    headerPhone.style.background = '';
     selec.classList.remove('popup__open');
     document.removeEventListener("keydown", (evt)=>{
         if(evt.key=='Escape')
@@ -101,7 +92,7 @@ function closePopup(selec){
         headerPhone.classList.remove('header__menu_phone-white');
     }
 };
-
+//Открыть закрыть попап
 buttonOpen.addEventListener('click',()=>{
     openPopup(popup);
 });
@@ -112,16 +103,12 @@ popup.addEventListener('click',(evt)=>{
     if(evt.target.classList[0]==='popup')
     closePopup(popup);
 })
-
-
+//Сабмит формы
 form.addEventListener('submit',(evt)=>{
     evt.preventDefault();
-
     closePopup(popup);
 })
-
-const bgBlur = document.querySelector('.bg-blur');
-
+// Затемнение экрана
 window.addEventListener('scroll',()=>{
     bgBlur.style.opacity = pageYOffset/350;
     if(bgBlur.style.opacity>0.99){
@@ -133,38 +120,34 @@ window.addEventListener('scroll',()=>{
     }
 
 })
-const kosmet = document.querySelectorAll('.header__menu_item');
-const indi = document.querySelectorAll('.header__menu_down-indik');
-const menu =Array.from(document.querySelectorAll('.header__menu_down'));
-
-
-
+//Ховер на меню
 function addHoverMenu(selecName,selecMenu,ind){
     selecName.addEventListener('mouseover',()=>{
         selecMenu.style.display='block';
+        selecName.querySelector('.header__menu_item-text').style.color='pink';
         const x = selecName.getBoundingClientRect().left
         ind.style.left= x+70+'px';
         ind.style.top=-11+'px';
     })
     selecName.addEventListener('mouseleave',()=>{
         selecMenu.style.display='none';
+        selecName.querySelector('.header__menu_item-text').style.color='black';
     })
 }
+//Присваивание всем пунктам меню
 for(var i=0;i<=2;i++){
     addHoverMenu(kosmet[i],menu[i],indi[i]);
 }
-const popupMenuPhone = document.querySelector('.popup__menu');
-const menuButtonPhone = document.querySelector('.header__menu_phone-button');
-const buttonOP = document.querySelector('.header__menu_phone-buttonClose');
+//Меню телефон
 menuButtonPhone.addEventListener('click',()=>{
     popupMenuPhone.classList.add('popup__open_phone');
-    headerPhone.classList.add('header__menu_phone-white');
+    headerPhone.style.background = 'white';
     buttonOP.classList.add('header__button_open');
     menuButtonPhone.classList.add('header__button_close');
 })
 buttonOP.addEventListener('click',()=>{
     popupMenuPhone.classList.remove('popup__open_phone');
-    headerPhone.classList.remove('header__menu_phone-white');
+    headerPhone.style.background = '';
     buttonOP.classList.remove('header__button_open');
     menuButtonPhone.classList.remove('header__button_close');
 })
