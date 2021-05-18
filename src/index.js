6398e002134aad560bc94f899339c93facb71f9e
Artem Morozov <moroz.20000@gmail.com>
2021-05-18 19:55:45 +0300

    new Commit

--- src/index.js
+++ src/index.js
@@ -1,177 +1,93 @@
 import './page/index.css';
 
-const buttonOpen = document.querySelector(".main__title_button");
+const boardLeft = document.querySelector(".left");
+const boardCenter = document.querySelector('.center');
+const boardRight = document.querySelector('.right');
+const button = document.querySelector('.main__button');
 const buttonClose = document.querySelector('.popup__buttonClose');
 const popup = document.querySelector('.popup');
-const form = document.querySelector('.popup__form');
-const popupButton = document.querySelector('.popup__button');
-const test =Array.from(form.querySelectorAll('.test'));
-const headerPhone = document.querySelector('.header__menu_phone');
-const popupMenuPhone = document.querySelector('.popup__menu');
-const menuButtonPhone = document.querySelector('.header__menu_phone-button');
-const buttonOP = document.querySelector('.header__menu_phone-buttonClose');
-const kosmet = document.querySelectorAll('.header__menu_item');
-const indi = document.querySelectorAll('.header__menu_down-indik');
-const menu =Array.from(document.querySelectorAll('.header__menu_down'));
-const bgBlur = document.querySelector('.bg-blur');
-const imputs = document.querySelectorAll('.popup__input');
-const check = document.querySelector('.popup__input_check');
-//Валидация
-function someInput(){
-    return !test.some((inp)=>!inp.validity.valid);
+const buttonSub = document.querySelector('.popup__button');
+const tasksName = document.querySelector('.tasksName');
+const tasksText = document.querySelector('.text');
+const tasksNamePeople = document.querySelector('.name');
+const tasksTime = document.querySelector('.time');
+const tasksColor = document.querySelector('.color');
+const tasksBoard = document.querySelectorAll('.popup__radio');
+const boards = Array.from(document.querySelectorAll('.main__board_column-tasks'));
+
+
+let drugElem
+
+
+function allowDrop(evt) {
+    evt.preventDefault();
+
 }
-function validate (elem,inp){
-    document.querySelectorAll('.popup__error');
-    if(elem.validity.valid){
-        const errors=document.querySelectorAll(`#${inp}-error`);
-        const goods = document.querySelectorAll(`#${inp}-good`)
-        goods[0].classList.add('popup__error_open');
-        goods[0].classList.remove('popup__error_hidden');
-        errors.forEach(element=>{
-            element.classList.remove('popup__error_open');
-            element.classList.add('popup__error_hidden');
-        })
-        
+function drop(evt){
+    evt.preventDefault();
+    const currentElement = evt.target;
+    const isMoveable = currentElement.classList.contains('tasks');
+    if (isMoveable) {
+        return;
     }
-    else{
-        const errors=document.querySelectorAll(`#${inp}-error`);
-        const goods = document.querySelectorAll(`#${inp}-good`)
-        errors.forEach(element=>{
-            element.classList.add('popup__error_open');
-            element.classList.remove('popup__error_hidden');
-        })
-    };
+    const bord = evt.target;
+    bord.appendChild(drugElem);
 }
-// Открытие формы и присваиваине обработчиков
-function openPopup(selec){
-    headerPhone.style.background = 'white';
-    if(window.innerWidth<=961){
-        headerPhone.classList.add('header__menu_phone-white');
-    }
-    selec.classList.add('popup__open');
-    document.addEventListener("keydown", (evt)=>{
-        if(evt.key=='Escape')
-        closePopup(popup);
-    });
-    imputs.forEach(element => {
-        element.addEventListener('blur',()=>{
-            const inp = element.classList;
-            if(someInput()){
-                console.log(popupButton);
-                popupButton.classList.remove('popup__button_disablet');
-                popupButton.removeAttribute("disabled");
-            }
-            else{
-                popupButton.classList.add('popup__button_disablet');
-                popupButton.setAttribute("disabled", true);
-            }
-            validate(element,inp[0]);
-        });
-    });
-    check.addEventListener('blur', ()=>{
-        if(someInput()){
-            console.log(popupButton);
-            popupButton.classList.remove('popup__button_disablet');
-            popupButton.removeAttribute("disabled");
-        }
-        else{
-            popupButton.classList.add('popup__button_disablet');
-            popupButton.setAttribute("disabled", true);
-        }
+
+function addCard(board,title,text,people,time,color){
+    const card = document.getElementById('card').content.cloneNode(true);
+    card.querySelector('.tasks__title').textContent=title;
+    card.querySelector('.tasks__text').textContent=text;
+    card.querySelector('.tasks__people').textContent=people;
+    card.querySelector('.tasks__time').textContent=time;
+    card.querySelector('.tasks').style.background = color;
+    card.querySelector('.tasks').draggable = true;
+    card.querySelector('.tasks').addEventListener('dragstart',(evt)=>{
+        evt.target.classList.add('selected');
+        drugElem = evt.target;
     })
-};
-//Закрытие формы
-function closePopup(selec){
-    headerPhone.style.background = '';
-    selec.classList.remove('popup__open');
-    document.removeEventListener("keydown", (evt)=>{
-        if(evt.key=='Escape')
-        closePopup(popup);
-    });
-    if(window.innerWidth<=961){
-        headerPhone.classList.remove('header__menu_phone-white');
+    card.querySelector('.tasks').addEventListener('dragend',(evt)=>{
+        evt.target.classList.remove('selected');
+    })
+    board.append(card);
+}
+
+function openPopup(pop){
+    pop.classList.add('popup__open');
+}
+
+function closePopup(pop){
+    pop.classList.remove('popup__open');
+}
+
+function checkRadio(){
+    if(tasksBoard[0].checked){
+        addCard(boardLeft,tasksName.value,tasksText.value,tasksNamePeople.value,tasksTime.value,tasksColor.value);
     }
-};
-//Открыть закрыть попап
-buttonOpen.addEventListener('click',()=>{
+    else if(tasksBoard[1].checked){
+        addCard(boardCenter,tasksName.value,tasksText.value,tasksNamePeople.value,tasksTime.value,tasksColor.value);
+    }
+    else if(tasksBoard[2].checked){
+        addCard(boardRight,tasksName.value,tasksText.value,tasksNamePeople.value,tasksTime.value,tasksColor.value);
+    }
+}
+
+buttonSub.addEventListener('click',()=>{
+    checkRadio();
+    console.log(tasksColor.value);
+    closePopup(popup);
+})
+button.addEventListener('click',()=>{
     openPopup(popup);
-});
+})
 buttonClose.addEventListener('click',()=>{
     closePopup(popup);
+})
+boards.forEach(element => {
+    element.addEventListener(('dragover'),(evt)=>{
+        allowDrop(evt);
+    })
+    element.addEventListener(('drop'),(evt)=>{
+        drop(evt);
+    })
 });
-popup.addEventListener('click',(evt)=>{
-    if(evt.target.classList[0]==='popup')
-    closePopup(popup);
-})
-//Сабмит формы
-form.addEventListener('submit',(evt)=>{
-    evt.preventDefault();
-    closePopup(popup);
-})
-// Затемнение экрана
-window.addEventListener('scroll',()=>{
-    bgBlur.style.opacity = pageYOffset/350;
-    if(bgBlur.style.opacity>0.99){
-        bgBlur.style.opacity=0.999;
-        headerPhone.classList.add('header__menu_phone-white');
-    }
-    else{
-        headerPhone.classList.remove('header__menu_phone-white');
-    }
-
-})
-//Ховер на меню
-function addHoverMenu(selecName,selecMenu,ind){
-    selecName.addEventListener('mouseover',()=>{
-        selecMenu.style.display='block';
-        selecName.querySelector('.header__menu_item-text').style.color='pink';
-        const x = selecName.getBoundingClientRect().left
-        ind.style.left= x+70+'px';
-        ind.style.top=-11+'px';
-    })
-    selecName.addEventListener('mouseleave',()=>{
-        selecMenu.style.display='none';
-        selecName.querySelector('.header__menu_item-text').style.color='black';
-    })
-}
-//Присваивание всем пунктам меню
-for(var i=0;i<=2;i++){
-    addHoverMenu(kosmet[i],menu[i],indi[i]);
-}
-//Меню телефон
-menuButtonPhone.addEventListener('click',()=>{
-    popupMenuPhone.classList.add('popup__open_phone');
-    headerPhone.style.background = 'white';
-    buttonOP.classList.add('header__button_open');
-    menuButtonPhone.classList.add('header__button_close');
-})
-buttonOP.addEventListener('click',()=>{
-    popupMenuPhone.classList.remove('popup__open_phone');
-    headerPhone.style.background = '';
-    buttonOP.classList.remove('header__button_open');
-    menuButtonPhone.classList.remove('header__button_close');
-})
-
-
-
-const icons = document.querySelectorAll('.header__icons_icon');
-
-for(var i=0;i<=5;i++){
-    const x = icons[i];
-    const y = icons[i+1];
-    const a = icons[i-1];
-    if(i===0||i%2===0){
-        x.addEventListener('mouseover',()=>{
-            x.classList.add('header__icons_hide');
-            y.classList.remove('header__icons_hide');
-        })
-    }
-    else{
-        x.addEventListener('mouseout',()=>{
-            x.classList.add('header__icons_hide');
-            a.classList.remove('header__icons_hide');
-        })
-    }
-}
-    
-
