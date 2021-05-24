f6ebbe6c5ad8dd2edd57b81af2c0d25cf582d501
Artem Morozov <moroz.20000@gmail.com>
2021-05-24 16:05:44 +0300

    commit fix

--- src/index.js
+++ src/index.js
@@ -15,26 +15,6 @@ const tasksColor = document.querySelector('.color');
 const tasksBoard = document.querySelectorAll('.popup__radio');
 const boards = Array.from(document.querySelectorAll('.main__board_column-tasks'));
 
-
-let drugElem
-
-
-
-function allowDrop(evt) {
-    evt.preventDefault();
-
-}
-function drop(evt){
-    evt.preventDefault();
-    const currentElement = evt.target;
-    const isMoveable = currentElement.classList.contains('tasks');
-    if (isMoveable) {
-        return;
-    }
-    const bord = evt.target;
-    bord.appendChild(drugElem);
-}
-
 function addCard(board,title,text,people,time,color){
     const card = document.getElementById('card').content.cloneNode(true);
     card.querySelector('.tasks__title').textContent=title;
@@ -42,17 +22,12 @@ function addCard(board,title,text,people,time,color){
     card.querySelector('.tasks__people').textContent=people;
     card.querySelector('.tasks__time').textContent=time;
     card.querySelector('.tasks').style.background = color;
-    card.querySelector('.tasks').draggable = true;
     card.querySelector('.tasks__delete').addEventListener(('click'),(evt)=>{
         evt.target.parentNode.parentNode.remove();
     });
-    card.querySelector('.tasks').addEventListener('dragstart',(evt)=>{
-        evt.target.classList.add('selected');
-        drugElem = evt.target;
-    })
-    card.querySelector('.tasks').addEventListener('dragend',(evt)=>{
-        evt.target.classList.remove('selected');
-    })
+    new Draggabilly(card.querySelector('.tasks'),{
+        containment: '.main__board_column'
+    });
     board.append(card);
 }
 
@@ -87,12 +62,5 @@ button.addEventListener('click',()=>{
 buttonClose.addEventListener('click',()=>{
     closePopup(popup);
 })
-boards.forEach(element => {
-    element.addEventListener(('dragover'),(evt)=>{
-        allowDrop(evt);
-    })
-    element.addEventListener(('drop'),(evt)=>{
-        drop(evt);
-    })
-});
 addCard(boardLeft,"Задание 1","Надо выполнить задание 1","Выполнит: Морозов Артем Алексеевич","с 21.02.13 по 22.03.14",'red');
+
